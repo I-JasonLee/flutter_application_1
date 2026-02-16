7e33e88da667979d71f12dcc3c3d66ee0cd4e594
Jason <1123150116@global.ac.id>
2026-02-16 17:37:32 +0700

    Future<void> onLoad() async{}

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -2,4 +2,9 @@ class FruitCatcherGame extends FlameGame {
   @override
   Color backgroundColor() => const Color(0xFF87CEEB);
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+  }
 }

de5ed1b0d021c1587b504e24feae088c8e3ff27e
Jason <1123150116@global.ac.id>
2026-02-16 17:42:35 +0700

    tambah import

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -1,3 +1,6 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
 class FruitCatcherGame extends FlameGame {
   @override
   Color backgroundColor() => const Color(0xFF87CEEB);

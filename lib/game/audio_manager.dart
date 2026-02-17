f93ee5195b969654b4736f73b17e0517c5e9487b
Jason <1123150116@global.ac.id>
2026-02-17 16:18:37 +0700

    factory AudioManager()

--- lib/game/audio_manager.dart
+++ lib/game/audio_manager.dart
@@ -3,4 +3,5 @@ import 'package:flame_audio/flame_audio.dart';
 class AudioManager {
   // Singleton pattern
   static final AudioManager _instance = AudioManager._internal();
+  factory AudioManager() => _instance;
 }

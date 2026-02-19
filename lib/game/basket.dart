818c46e124a16289cab6f7eb5223251898ae29a4
Jason <1123150116@global.ac.id>
2026-02-19 16:49:09 +0700

    tambah draw basket body

--- lib/game/basket.dart
+++ lib/game/basket.dart
@@ -2,14 +2,35 @@ import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
-class Basket extends PositionComponent with HasGameRef, CollisionCallbacks {
+class Basket extends PositionComponent
+  with HasGameRef, CollisionCallbacks {
+
   Basket() : super(size: Vector2(80, 60));
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    position = Vector2(gameRef.size.x / 2,
-    gameRef.size.y - 100);
+
+    position = Vector2(
+      gameRef.size.x / 2,
+      gameRef.size.y - 100
+    );
+
     anchor = Anchor.center;
+
     add(RectangleHitbox());
-    }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    final paint = Paint()
+    ..color = Colors.brown
+    ..style = PaintingStyle.fill;
+
+    // Draw basket body
+    final rect = RRect.fromRectAndRadius(
+      Rect.fromLTWH(0, 0, size.x, size.y),
+      const Radius.circular(10),
+    );
